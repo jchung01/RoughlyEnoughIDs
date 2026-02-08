7021ce6446d2850f9f231d6212a4804c479d31c7
jchung01 <jjchung276@gmail.com>
2026-02-08 15:01:45 -0800

    Refactor mod names

--- src/main/java/org/dimdev/jeid/core/JEIDMixinLoader.java
+++ src/main/java/org/dimdev/jeid/core/JEIDMixinLoader.java
@@ -1,111 +1,41 @@
 package org.dimdev.jeid.core;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.StringJoiner;
 
-import net.minecraftforge.fml.common.Loader;
-
+import org.dimdev.jeid.jeid.Tags;
+import org.dimdev.jeid.util.Mods;
 import zone.rong.mixinbooter.ILateMixinLoader;
 
 public class JEIDMixinLoader implements ILateMixinLoader {
     public List<String> getMixinConfigs() {
         List<String> configs = new ArrayList<>();
 
+        // Common configs
+        Arrays.stream(Mods.values())
+                .filter(Mods::isLoaded)
+                .map(mod -> getMixinConfigName(mod, false))
+                .forEach(configs::add);
+
+        // Client configs
         if (JEIDLoadingPlugin.isClient) {
-            if (Loader.isModLoaded("advancedrocketry")) {
-                configs.add("mixins.jeid.advancedrocketry.client.json");
+            if (Mods.ADVANCED_ROCKETRY.isLoaded()) {
+                configs.add(getMixinConfigName(Mods.ADVANCED_ROCKETRY, true));
             }
         }
-        if (Loader.isModLoaded("abyssalcraft")) {
-            configs.add("mixins.jeid.abyssalcraft.json");
-        }
-        if (Loader.isModLoaded("advancedrocketry")) {
-            configs.add("mixins.jeid.advancedrocketry.json");
-        }
-        if (Loader.isModLoaded("atum")) {
-            configs.add("mixins.jeid.atum.json");
-        }
-        if (Loader.isModLoaded("biomesoplenty")) {
-            configs.add("mixins.jeid.biomesoplenty.json");
-        }
-        if (Loader.isModLoaded("biomestaff")) {
-            configs.add("mixins.jeid.biomestaff.json");
-        }
-        if (Loader.isModLoaded("biometweaker")) {
-            configs.add("mixins.jeid.biometweaker.json");
-        }
-        if (Loader.isModLoaded("bookshelf")) {
-            configs.add("mixins.jeid.bookshelf.json");
-        }
-        if (Loader.isModLoaded("compactmachines3")) {
-            configs.add("mixins.jeid.compactmachines.json");
-        }
-        if (Loader.isModLoaded("creepingnether")) {
-            configs.add("mixins.jeid.creepingnether.json");
-        }
-        if (Loader.isModLoaded("chunkpregenerator")) {
-            configs.add("mixins.jeid.chunkpregenerator.json");
-        }
-        if (Loader.isModLoaded("cubicchunks")) {
-            configs.add("mixins.jeid.cubicchunks.json");
-        }
-        if (Loader.isModLoaded("cyclopscore")) {
-            configs.add("mixins.jeid.cyclopscore.json");
-        }
-        if (Loader.isModLoaded("extrautils2")) {
-            configs.add("mixins.jeid.extrautils2.json");
-        }
-        if (Loader.isModLoaded("gaiadimension")) {
-            configs.add("mixins.jeid.gaiadimension.json");
-        }
-        if (Loader.isModLoaded("geographicraft")) {
-            configs.add("mixins.jeid.geographicraft.json");
-        }
-        if (Loader.isModLoaded("hammercore")) {
-            configs.add("mixins.jeid.hammercore.json");
-        }
-        if (Loader.isModLoaded("journeymap")) {
-            configs.add("mixins.jeid.journeymap.json");
-        }
-        if (Loader.isModLoaded("kathairis")) {
-            configs.add("mixins.jeid.kathairis.json");
-        }
-        if (Loader.isModLoaded("moreplanets")) {
-            configs.add("mixins.jeid.moreplanets.json");
-        }
-        if (Loader.isModLoaded("mystcraft")) {
-            configs.add("mixins.jeid.mystcraft.json");
-        }
-        if (Loader.isModLoaded("naturescompass")) {
-            configs.add("mixins.jeid.naturescompass.json");
-        }
-        if (Loader.isModLoaded("rtg")) {
-            configs.add("mixins.jeid.rtg.json");
-        }
-        if (Loader.isModLoaded("srparasites")) {
-            configs.add("mixins.jeid.srparasites.json");
-        }
-        if (Loader.isModLoaded("thaumcraft")) {
-            configs.add("mixins.jeid.thaumcraft.json");
-        }
-        if (Loader.isModLoaded("thebetweenlands")) {
-            configs.add("mixins.jeid.thebetweenlands.json");
-        }
-        if (Loader.isModLoaded("tofucraft")) {
-            configs.add("mixins.jeid.tofucraft.json");
-        }
-        if (Loader.isModLoaded("tropicraft")) {
-            configs.add("mixins.jeid.tropicraft.json");
-        }
-        if (Loader.isModLoaded("twilightforest")) {
-            configs.add("mixins.jeid.twilightforest.json");
-        }
-        if (Loader.isModLoaded("worldedit")) {
-            configs.add("mixins.jeid.worldedit.json");
-        }
-        if (Loader.isModLoaded("wyrmsofnyrus")) {
-            configs.add("mixins.jeid.wyrmsofnyrus.json");
-        }
+
         return configs;
+    }
+
+    private String getMixinConfigName(Mods mod, boolean clientOnly) {
+        StringJoiner joiner = new StringJoiner(".");
+        joiner.add("mixins").add(Tags.MOD_ID).add(mod.modId);
+        if (clientOnly) {
+            joiner.add("client");
+        }
+        joiner.add("json");
+        return joiner.toString();
     }
 }
---
+++ src/main/java/org/dimdev/jeid/util/Mods.java
@@ -0,0 +1,52 @@
+package org.dimdev.jeid.util;
+
+import net.minecraft.util.ResourceLocation;
+
+import net.minecraftforge.fml.common.Loader;
+import org.dimdev.jeid.JEID;
+
+public enum Mods {
+    ADVANCED_ROCKETRY("advancedrocketry"),
+    ABYSSALCRAFT("abyssalcraft"),
+    ATUM("atum"),
+    BIOMES_O_PLENTY("biomesoplenty"),
+    BIOME_STAFF("biomestaff"),
+    BIOME_TWEAKER("biometweaker"),
+    BOOKSHELF("bookshelf"),
+    COMPACT_MACHINES("compactmachines3"),
+    CREEPING_NETHER("creepingnether"),
+    CHUNK_PREGENERATOR("chunkpregenerator"),
+    CUBIC_CHUNKS("cubicchunks"),
+    CYCLOPS_CORE("cyclopscore"),
+    EXTRA_UTILS("extrautils2"),
+    GAIA_DIMENSION("gaiadimension"),
+    GEOGRAPHICRAFT("geographicraft"),
+    HAMMERLIB("hammercore"),
+    JOURNEYMAP("journeymap"),
+    KATHAIRIS("kathairis"),
+    MORE_PLANETS("moreplanets"),
+    MYSTCRAFT("mystcraft"),
+    NATURES_COMPASS("naturescompass"),
+    REALISTIC_TERRAIN_GEN("rtg"),
+    SCAPE_RUN_PARASITES("srparasites"),
+    THAUMCRAFT("thaumcraft"),
+    BETWEENLANDS("thebetweenlands"),
+    TOFUCRAFT("tofucraft"),
+    TROPICRAFT("tropicraft"),
+    TWILIGHT_FOREST("twilightforest"),
+    WORLD_EDIT("worldedit"),
+    WYRMS_OF_NYRUS("wyrmsofnyrus"),
+    ;
+
+    public final String modId;
+    private final ResourceLocation registryKey;
+
+    Mods(String modId) {
+        this.modId = modId;
+        registryKey = new ResourceLocation(JEID.MODID, modId);
+    }
+
+    public boolean isLoaded() {
+        return Loader.isModLoaded(modId);
+    }
+}
