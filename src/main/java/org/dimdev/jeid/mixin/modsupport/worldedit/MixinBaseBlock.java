6c361bc58eb51ab87125565730afc41cbf30ee35
jchung01 <jjchung276@gmail.com>
2026-02-18 15:12:31 -0800

    Remove api usage in MixinBaseBlock

--- src/main/java/org/dimdev/jeid/mixin/modsupport/worldedit/MixinBaseBlock.java
+++ src/main/java/org/dimdev/jeid/mixin/modsupport/worldedit/MixinBaseBlock.java
@@ -2,7 +2,6 @@ package org.dimdev.jeid.mixin.modsupport.worldedit;
 
 import com.llamalad7.mixinextras.expression.Expression;
 import com.sk89q.worldedit.blocks.BaseBlock;
-import org.dimdev.jeid.api.BiomeApi;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -24,7 +23,7 @@ public class MixinBaseBlock {
 
     @ModifyConstant(method = "internalSetId", constant = @Constant(intValue = 4095))
     private int reid$getMaxBlockId(int oldValue) {
-        return BiomeApi.INSTANCE.getMaxBiomeId();
+        return Integer.MAX_VALUE - 1;
     }
 
     @Expression("@((short) ?)")
